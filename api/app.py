7210d558d79c6472403c55775574c21b5a77b3d3
Andrew Gilliam <gilliamandrew22@gmail.com>
2026-09-12 21:14:57 -0400

    Bind backend for Android emulator

--- api/app.py
+++ api/app.py
@@ -841,4 +841,4 @@ def read_upstream_error(response):
 
 
 if __name__ == "__main__":
-    create_app().run(debug=True)
+    create_app().run(host="0.0.0.0", port=int(os.environ.get("PORT", "5000")), debug=True)
